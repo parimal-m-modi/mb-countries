6251cbf4bb880ac3658afe59835d562675d3e386
Parimal Modi <parimalmo@cybage.com>
2021-02-27 10:08:24 +0530

    Initial commit

---
+++ MBCountries/MBCountries/Countries/View/CountriesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MBCountries
+//
+//  Created by Parimal Modi on 24/02/21.
+//
+
+import UIKit
+
+class CountriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
